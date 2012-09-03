879ce32928aee0d02aba019f13dd1016aa0a5ea4
Mattias Putman <mattias.putman@gmail.com>
2012-09-03 22:33:42 +0200

    fix minitest to miniTest; Performance improvement with inherited; bring back load check

--- lib/dominance/minitest.rb
+++ lib/dominance/minitest.rb
@@ -1,18 +1,16 @@
-module Minitest
+module MiniTest
+  raise 'Dominance for minitest/spec needs minitest/spec to be loaded' unless const_defined?(:Spec)
 
   # Add some functionality to minitest/spec.
   class Spec
+    @@descendants = []
 
     # New it! method that dominates one test.
     # Could be implemented better.
     def self.it! *args, &block
       # First we need to nuke all test methods that were already defined.
-      # Herefor we find all decendants of Minitest::Spec.
-      test_classes = ObjectSpace.each_object(Class).select do |klass|
-        klass < MiniTest::Spec
-      end
-      # For eahc of this descendants, nuke the test methods.
-      test_classes.each(&:nuke_test_methods!)
+      @@descendants.each(&:nuke_test_methods!)
+      @@descendants = []
       # Now define the test method for the dominating test.
       self.it *args, &block
       # Now make sure that no test will be defined anymore by overwriting `it`.
@@ -21,6 +19,12 @@ module Minitest
           # Empty on purpose.
         end
       end
+    end
+
+    # We need to gather all descendants of Minitest::Spec in case their tests need to be nuked.
+    def self.inherited test_class
+      super
+      @@descendants << test_class
     end
   end
 end
